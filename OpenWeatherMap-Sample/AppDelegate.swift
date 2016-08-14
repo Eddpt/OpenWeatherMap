8a19a009c5f42aa3056f8fff4e59044c0750057a
Eddpt <Eddpt@users.noreply.github.com>
2016-08-14 15:47:53 +0100

    Initial project structure

---
+++ OpenWeatherMap-Sample/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  OpenWeatherMap-Sample
+//
+//  Created by Eddpt on 14/08/2016.
+//  Copyright © 2016 test. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+}
+
---
+++ OpenWeatherMap-Sample/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  OpenWeatherMap-Sample
+//
+//  Created by Eddpt on 14/08/2016.
+//  Copyright © 2016 test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
